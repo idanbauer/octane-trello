78c1f39f1358582b2a735684a38c7295f68fa834
Idan Bauer <idan.bauer@hp.com>
2016-06-19 00:45:36 +0300

    fixed webhook

--- app.js
+++ app.js
@@ -1,6 +1,8 @@
 //var trelloClient = require('./trello-client.js')();
 require('./logger.js');
-require('./server.js');
-var trelloClient = require('./trello-client.js');
 
-trelloClient.registerWebhookd('57605a53fc1d296a5abbc54d');
+var trelloClient = require('./trello-client.js')();
+trelloClient.registerWebHook('57605a53fc1d296a5abbc54d');
+
+require('./server.js');
+
